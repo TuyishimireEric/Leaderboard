5360fcb412e5a2373cbf166577782a2c3b6920e7
Tuyishimire Eric <tuyishimireericc@gmail.com>
2022-08-22 11:46:12 +0200

    Update add-player.js

--- src/modules/add-player.js
+++ src/modules/add-player.js
@@ -7,14 +7,16 @@ const clearFields = () => {
 
 const showPlayer = async () => {
   const players = await getPlayer();
+  const position = 1;
   players.result.sort((a, b) => b.score - a.score);
   const list = document.querySelector('.Leaderboard');
   players.result.forEach((player) => {
     const addedplayer = document.createElement('tr');
     addedplayer.innerHTML = `
-      <td class="cols">${player.user}: ${player.score}</td>
+      <td class="cols">${position} ${player.user}: ${player.score}</td>
       `;
     list.appendChild(addedplayer);
+    position += 1;
   });
 };
 
